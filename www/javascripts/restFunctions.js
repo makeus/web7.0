c9770eb33c48b9b853337c1b3dcf690a524dd3aa
ktj <julius.raiha@helsinki.fi>
2013-05-22 12:13:16 +0300

    Refactored restFunctions, removed stuff that arent rest related at all

--- www/javascripts/restFunctions.js
+++ www/javascripts/restFunctions.js
@@ -1,35 +1,15 @@
 var url =  "https://www.dliv.in/rest/";
-function saveToken(data){
-	localStorage.setItem('authtoken',data.authtoken);
-}
-function saveDL_id(data){
-	localStorage.setItem('DL_id',data.DL_id);
-}
+
 function loginRest(username, password){
-	localStorage.removeItem('authtoken');
-	localStorage.removeItem('DL_id');
 	$.ajax(
 	{
 		url: url+"Authtoken",
 		dataType: "json",
 		async: false,
 		data: {u: username, p: password},
-		success: function(data){
-			if(data.success!="0"){
-				saveToken(data);
-				saveDL_id(data);
-			}
-		}
+		success: checkLogin
 	});
 }
-function getToken(){
-	return localStorage.getItem('authtoken');
-}
-function getDL_id(){
-	return localStorage.getItem('DL_id');
-}
-
-
 
 var infoStream;
 //todo add limit & offset when they work
