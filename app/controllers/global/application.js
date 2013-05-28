2c347d41268ddaa331858a9da6045a2f47494570
ktj <julius.raiha@helsinki.fi>
2013-05-28 10:58:28 +0300

    application.js refactor fix

--- app/controllers/global/application.js
+++ app/controllers/global/application.js
@@ -2,3 +2,149 @@ function getHistory(own_id, dl_id,authToken){
 	var opts={'dl_id':dl_id,'auth':authToken,'uid':own_id};
 	return getHistoryRest(opts);
 }
+var url =  "https://www.dliv.in/rest/";
+var status = 0;
+
+
+function getURLParameter(name) {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
+}
+
+function pushView(name, page) {
+	if (page == undefined)
+		page = "index.html";
+
+	if (isSteroids()) {
+		var webView = new steroids.views.WebView("views/"+name+"/"+page);
+		steroids.layers.push(webView);
+	} else {
+		window.location.href = "../"+name+"/"+page;
+	}
+}
+var userInfo;
+function getUserInfoRest(opts){
+	status = 0;
+	userInfo="";
+	$.ajax(
+	{
+		url: url+"dlid",
+		dataType: "json",
+		global: false,
+		async: false,
+		data: opts,
+		success: function(data) {
+			userInfo = data;
+			success(data);
+		},
+		error: function(data) {
+			userInfo = data;
+			error(data);
+		}
+	});
+	return userInfo;
+}
+
+
+
+function loginRest(username, password){
+	status = 0;
+	$.ajax(
+	{
+		url: url+"Authtoken",
+		dataType: "json",
+		global: false,
+		async: false,
+		data: {u: username, p: password},
+		success: function(data){
+			success(data);
+			if(getStatus() == 1) {
+					saveToken(data);
+					saveDL_id(data);
+			}
+		},
+		error: error
+	});
+}
+
+var infoStream;
+
+function getActivityStream(userId,authToken,offset,limit,types){
+	status = 0;
+	infoStream="";
+	$.ajax(
+	{
+		url: url+"stream",
+		dataType: "json",
+		global: false,
+		async: false,
+		data: {
+			uid: userId, 
+			auth: authToken, 
+			offset: offset, 
+			limit: limit, 
+			types: types
+		},
+		success: function(data) {
+			infoStream = data;
+			success(data);
+		},
+		error: function(data) {
+			infoStream = data;
+			error(data);
+		}
+	});
+	return infoStream;
+}
+
+function addActivity(opts) {
+	status = 0;
+	$.ajax(
+	{
+		url: url+"stream",
+		type: "POST",
+		global: false,
+		async: false,
+		dataType: 'JSON', 
+		data: opts,
+		success: success,
+		error: error
+	});
+}
+
+function success(data) {
+	if(data != undefined && data.success != 0) {
+		status = 1;
+	} else {
+		status = -1;
+	}
+}
+
+function error(data, t, m) {
+	if(t == "timeout") {
+		alert("TIMEOUT");
+	} else {
+		alert("ERROR " + t + ": " + m);
+	}
+	status = data.status;
+}
+
+function getStatus() {
+  return status;
+}
+
+function setStatus(uusi) {
+	status = uusi;
+}
+
+function saveToken(data){
+	localStorage.setItem('authtoken',data.authtoken);
+}
+function saveDL_id(data){
+	localStorage.setItem('DL_id',data.DL_id);
+}
+function getToken(){
+	return localStorage.getItem('authtoken');
+}
+function getDL_id(){
+	return localStorage.getItem('DL_id');
+}
