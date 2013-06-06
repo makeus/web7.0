8dc054e1af3970845d9244b34bc2d213a4462e05
ktj <julius.raiha@helsinki.fi>
2013-06-06 10:44:33 +0300

    console.logit pois

--- app/controllers/global/application.js
+++ app/controllers/global/application.js
@@ -28,7 +28,6 @@ function addLiListener(){
 
 function getMessageInfo(id){
 	var opts = {'uid': getDL_id(), 'auth': getToken(), activity_id:id};
-	console.log(opts);
 	var url = "stream";
 	var item = rest(opts,url,function(data) {
 			result = data;
@@ -38,7 +37,6 @@ function getMessageInfo(id){
 			result = data;
 			error(data);
 		});
-	console.log(item);
 	return item;
 }
 
