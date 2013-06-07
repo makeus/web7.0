035c7c90fc57fcfaa0cddf23ab561ad0a6f84b98
ktj <julius.raiha@helsinki.fi>
2013-06-07 12:33:34 +0300

    testi ipage

--- app/controllers/IPage.js
+++ app/controllers/IPage.js
@@ -46,6 +46,7 @@ function setAddCommentEvent(){
 
 function parseMessage(){
     info = getMessageInfo(iPageID,getURLParameter("uid"));
+    if(info==undefined){return;};
     $("#messageContent").append(getSubject(info));
     if (info.content==""){
         $("#ipageContent").hide();
