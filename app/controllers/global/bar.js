aa731fbf10b2362d35514e95bca7da1a54aa0389
ktapulkk <kristian.pulkkinen@cs.helsinki.fi>
2013-06-19 15:49:41 +0300

    relaatiotyyppilistaus

--- app/controllers/global/bar.js
+++ app/controllers/global/bar.js
@@ -36,8 +36,7 @@ bar = {
 		});
 		$("#relationsButton").click(function(){
 			$( "#rightpanel" ).panel( "open" );
-
-		})
+		});
 		$("#searchButton").click(function() {
    			view.push("search");
         });
@@ -48,26 +47,76 @@ bar = {
         	view.push("login");
         });
 	},
+
+
+
 	showSettingsList: function(){
 		setTimeout(function(){
-			$("#wholePage").click(function(){bar.hideSettingsList();});
+			$("body").click(function(elem){
+				if (!isSettingsList(elem)) {
+					bar.hideSettingsList();
+				}
+			});
 		},500);
 		$("#settingsList").css("display","table");
 		var x = $("#settingsButton").position().left;
 		$("#settingsList").css("left",x+"px");
-		$("#settingsButton").unbind();
+		$("#settingsButton").off();
 		$("#settingsButton").click(function(){
 			bar.hideSettingsList();
+			bar.hideRelationsList();
 		});
+		
+
+		var dlid = getParameter("dlid");
+
+		if (dlid !== undefined) {
+			$("#connectToMeButton").show();
+			$("#connectToMeButton").off();
+			$("#connectToMeButton").click(function(){
+	        	bar.showRelationsList();
+	        });
+		} else {
+			$("#connectToMeButton").hide();
+		}
 	},
 	hideSettingsList: function(){
-		$("#wholePage").unbind();
+		$("body").off();
 		$("#settingsList").css("display","none");
-		$("#settingsButton").unbind();
+		$("#settingsButton").off();
 		$("#settingsButton").click(function(){
 			bar.showSettingsList();
 		});
+		bar.hideRelationsList();
 	},
+
+
+	showRelationsList: function() {
+		$("#connectToMeButton").off();
+		$("#connectToMeButton").click(function(){
+        	bar.hideRelationsList();
+        });
+
+		var relations = ["-", "User", "Service provider", "Owner", "Child", "Parent", "Friend"];
+
+		$.each(relations, function(i, item) {
+			var id = item.replace(" ", "_");
+			$("#relationsList").append("<li id='"+id+"' ><div>"+item+"</div></li>");
+			$("#"+id).click(function() {
+				bar.hideSettingsList();
+				// Tähän relaatiofunktiohässäkkä
+			});
+		});
+
+		$("#relationsList").css("display", "table");
+	},
+	hideRelationsList: function() {
+		$("#relationsList").empty();
+		$("#connectToMeButton").off();
+	},
+
+
+
 	showSearch: function(){
 		$("#searchArea").show();
 		$("#ownPictureButton").hide();
@@ -97,3 +146,15 @@ bar = {
 }
 
 
+function isSettingsList(elem) {
+	var target = elem.target;
+
+	while (target !== null) {
+		if (target.id != undefined)
+			if (target.id === "settingsList")
+				return true;
+
+		target = target.parentElement;
+	}
+	return false;
+}
